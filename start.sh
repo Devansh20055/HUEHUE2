8414012de6db2ed2b9c3db228718e99b375bffea
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-01-31 16:50:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Devansh20055/HUEHUE2.git /HUEHUE2
+  git clone https://github.com/Devansh20055/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /HUEHUE2
 fi
 cd /HUEHUE2
 pip3 install -U -r requirements.txt
