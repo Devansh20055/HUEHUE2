d059ebe5c9b22746041b1672585e82a33509ca58
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-01-31 16:47:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/Devansh20055/HUEHUE2.git /HUEHUE2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /HUEHUE2
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
 cd /HUEHUE2
 pip3 install -U -r requirements.txt
