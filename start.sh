6d4b621f4dd722b65f0704f10f36788380d36c74
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-01-31 16:39:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Devansh20055/filter-v-5.git /filter-v-5
+  git clone https://github.com/Devansh20055/HUEHUE2.git /HUEHUE2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /filter-v-5
+  git clone $UPSTREAM_REPO /HUEHUE2
 fi
-cd /filter-v-5
+cd /HUEHUE2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
